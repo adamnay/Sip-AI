94fed7abc3658f43ed4f0d6bb3069eaa82ef09e4
Adam Nayowitz <amn196@scarletmail.rutgers.edu>
2026-03-25 09:58:27 -0400

    hardcode supabase keys to fix build

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string ?? 'https://placeholder.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string ?? 'placeholder';
+const supabaseUrl = 'https://iawrnzqubhzphlouriva.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlhd3JuenF1Ymh6cGhsb3VyaXZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0NDI0NDgsImV4cCI6MjA5MDAxODQ0OH0.Vzt6jIHuBtwsLyJVakvDgupEhnDSsSm_koZlJFMCsQA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
